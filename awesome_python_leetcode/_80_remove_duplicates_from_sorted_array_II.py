0e50dca03d32cc8ca9127831a56ef478a3a54e6f
Dennis Jabs <dennis-jabs@outlook.de>
2025-03-09 17:19:21 +0100

    D. J.: - Added leetcode problem and solution for 80. Remove Duplicates from Sorted Array II

---
+++ awesome_python_leetcode/_80_remove_duplicates_from_sorted_array_II.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        Given an integer array nums sorted in non-decreasing order, remove some duplicates
+        in-place such that each unique element appears at most twice.
+        The relative order of the elements should be kept the same.
+
+        Since it is impossible to change the length of the array in some languages, you must instead
+        have the result be placed in the first part of the array nums.
+        More formally, if there are k elements after removing the duplicates, then the first k elements
+        of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+        Return k after placing the final result in the first k slots of nums.
+
+        Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+        """
+        left = 1
+        right = 1
+        length = 1
+        second_val = True
+        val_before = nums[0]
+        while right < len(nums):
+            if second_val and val_before == nums[right]:
+                nums[left] = nums[right]
+                val_before = nums[left]
+                second_val = False
+                left += 1
+                right += 1
+                length += 1
+            elif val_before == nums[right]:
+                right += 1
+            else:
+                nums[left] = nums[right]
+                val_before = nums[left]
+                second_val = True
+                left += 1
+                right += 1
+                length += 1
+        return length
---
+++ tests/test_80_remove_duplicates_from_sorted_array_II.py
@@ -0,0 +1,17 @@
+from typing import List
+
+import pytest
+
+from awesome_python_leetcode._80_remove_duplicates_from_sorted_array_II import Solution
+
+
+@pytest.mark.parametrize(
+    argnames=["nums", "expected"],
+    argvalues=[
+        ([1, 1, 1, 2, 2, 3], [1, 1, 2, 2, 3]),
+        ([0, 0, 1, 1, 1, 1, 2, 3, 3], [0, 0, 1, 1, 2, 3, 3]),
+    ],
+)
+def test_func(nums: List[int], expected: List[int]):
+    length = Solution().removeDuplicates(nums)
+    assert nums[:length] == expected

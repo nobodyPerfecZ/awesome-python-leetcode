58b324f3138b8c5205fa163d94dd769cc0949e13
Dennis Jabs <dennis-jabs@outlook.de>
2025-03-09 17:07:54 +0100

    D. J.: - Added leetcode problem and solution for 26. Remove Duplicates from Sorted Array

---
+++ tests/test_26_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,17 @@
+from typing import List
+
+import pytest
+
+from awesome_python_leetcode._26_remove_duplicates_from_sorted_array import Solution
+
+
+@pytest.mark.parametrize(
+    argnames=["nums", "expected"],
+    argvalues=[
+        ([1, 1, 2], [1, 2]),
+        ([0, 0, 1, 1, 1, 2, 2, 3, 3, 4], [0, 1, 2, 3, 4]),
+    ],
+)
+def test_func(nums: List[int], expected: List[int]):
+    length = Solution().removeDuplicates(nums)
+    assert nums[:length] == expected

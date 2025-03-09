0adf52390828e6d2f226ed67b560c0f87aa47ec7
Dennis Jabs <dennis-jabs@outlook.de>
2025-03-09 16:26:25 +0100

    D. J.: - Added leetcode problem and solution for 88. Merge Sorted Array

---
+++ awesome_python_leetcode/_88_merge_sorted_array.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+        and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+        Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+        The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+        To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+        and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+        """
+        midx = m - 1
+        nidx = n - 1
+        right = m + n - 1
+
+        while nidx >= 0:
+            if midx >= 0 and nums1[midx] > nums2[nidx]:
+                nums1[right] = nums1[midx]
+                midx -= 1
+            else:
+                nums1[right] = nums2[nidx]
+                nidx -= 1
+            right -= 1
---
+++ tests/test_88_merge_sorted_array.py
@@ -0,0 +1,18 @@
+from typing import List
+
+import pytest
+
+from awesome_python_leetcode._88_merge_sorted_array import Solution
+
+
+@pytest.mark.parametrize(
+    argnames=["nums1", "m", "nums2", "n", "expected"],
+    argvalues=[
+        ([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3, [1, 2, 2, 3, 5, 6]),
+        ([1], 1, [], 0, [1]),
+        ([0], 0, [1], 1, [1]),
+    ],
+)
+def test_func(nums1: List[int], m: int, nums2: List[int], n: int, expected: List[int]):
+    Solution().merge(nums1, m, nums2, n)
+    assert nums1 == expected

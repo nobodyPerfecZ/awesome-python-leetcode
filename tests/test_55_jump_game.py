f06be309911d06ef9b687e3630ad8e84f291faaa
Dennis Jabs <dennis-jabs@outlook.de>
2025-03-10 10:13:34 +0100

    D. J.: - Added the leetcode problem and solution for 55. Jump Game

---
+++ tests/test_55_jump_game.py
@@ -0,0 +1,17 @@
+from typing import List
+
+import pytest
+
+from awesome_python_leetcode._55_jump_game import Solution
+
+
+@pytest.mark.parametrize(
+    argnames=["nums", "expected"],
+    argvalues=[
+        ([2, 3, 1, 1, 4], True),
+        ([3, 2, 1, 0, 4], False),
+    ],
+)
+def test_func(nums: List[int], expected: List[int]):
+    can_jump = Solution().canJump(nums)
+    assert can_jump == expected

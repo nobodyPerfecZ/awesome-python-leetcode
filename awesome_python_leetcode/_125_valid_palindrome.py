21fbaf48d17f828d9b43dd0dbfe343671c2b38d6
Dennis Jabs <dennis-jabs@outlook.de>
2025-03-10 12:19:15 +0100

    D. J.: - Added documentation for the leetcode problem 125 Valid Palindrome

--- awesome_python_leetcode/_125_valid_palindrome.py
+++ awesome_python_leetcode/_125_valid_palindrome.py
@@ -1,5 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        """
+        A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and
+        removing all non-alphanumeric characters, it reads the same forward and backward.
+        Alphanumeric characters include letters and numbers.
+
+        Given a string s, return true if it is a palindrome, or false otherwise.
+        """
         s = s.lower()
         left, right = 0, len(s) - 1
         while left < right:

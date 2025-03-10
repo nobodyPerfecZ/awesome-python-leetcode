aedaf89750a0386c85d040dcfcb5c213d0567790
Dennis Jabs <dennis-jabs@outlook.de>
2025-03-10 12:20:27 +0100

    D. J.: - Added the leetcode problem and solution for 392 Is Subsequence

---
+++ awesome_python_leetcode/_392_is_subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        """
+        Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+        A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
+        of the characters without disturbing the relative positions of the remaining characters.
+        (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+        """
+        left_s, right_s = 0, len(s)
+        left_t, right_t = 0, len(t)
+        while left_s < right_s and left_t < right_t:
+            if s[left_s] == t[left_t]:
+                left_s += 1
+            left_t += 1
+        return left_s == right_s
---
+++ tests/test_392_is_subsequence.py
@@ -0,0 +1,15 @@
+import pytest
+
+from awesome_python_leetcode._392_is_subsequence import Solution
+
+
+@pytest.mark.parametrize(
+    argnames=["s", "t", "expected"],
+    argvalues=[
+        ("abc", "ahbgdc", True),
+        ("axc", "ahbgdc", False),
+    ],
+)
+def test_func(s: str, t: str, expected: bool):
+    is_subsequence = Solution().isSubsequence(s, t)
+    assert is_subsequence == expected

9f278e187cbe367f0a80430b66be87a4616d5aab
Dennis Jabs <dennis-jabs@outlook.de>
2025-03-12 12:25:56 +0100

    D. J.: - Added the leetcode problem and solution for 155 Min Stack

---
+++ awesome_python_leetcode/_155_min_stack.py
@@ -0,0 +1,42 @@
+class MinStack:
+    """
+    Design a stack that supports push, pop, top, and retrieving the
+    minimum element in constant time.
+
+    Implement the MinStack class:
+    - MinStack() initializes the stack object.
+    - void push(int val) pushes the element val onto the stack.
+    - void pop() removes the element on the top of the stack.
+    - int top() gets the top element of the stack.
+    - int getMin() retrieves the minimum element in the stack.
+
+    You must implement a solution with O(1) time complexity for each function.
+    """
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            last_min_val = self.getMin()
+            min_val = min(last_min_val, val)
+            self.stack.append((val, min_val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
---
+++ tests/test_155_min_stack.py
@@ -0,0 +1,12 @@
+from awesome_python_leetcode._155_min_stack import MinStack
+
+
+def test_func():
+    min_stack = MinStack()
+    min_stack.push(-2)
+    min_stack.push(0)
+    min_stack.push(-3)
+    assert min_stack.getMin() == -3
+    min_stack.pop()
+    assert min_stack.top() == 0
+    assert min_stack.getMin() == -2

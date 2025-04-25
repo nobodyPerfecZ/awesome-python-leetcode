0e1fbcdde7a7f336290452ae5b6e8b5d23a1aa9a
Dennis Jabs <dennis-jabs@outlook.de>
2025-04-25 13:34:18 +0200

    D. J.: - Added the Binary Search (BS) Solution for Leetcode problem 300

--- awesome_python_leetcode/_300_longest_increasing_subsequence.py
+++ awesome_python_leetcode/_300_longest_increasing_subsequence.py
@@ -1,10 +1,11 @@
+import bisect
 from typing import List
 
 
 class Solution:
     """Base class for all LeetCode Problems."""
 
-    def lengthOfLIS(self, nums: List[int]) -> int:
+    def lengthOfLISBS(self, nums: List[int]) -> int:
         """
         Given an integer array nums, return the length of the longest strictly
         increasing subsequence.
@@ -12,6 +13,25 @@ class Solution:
         A subsequence is an array that can be derived from another array by deleting
         some or no elements without changing the order of the remaining elements.
         """
+        # Binary Search Solution: O(n log n)
+        seq = [nums[0]]
+        for k in range(1, len(nums)):
+            if nums[k] > seq[-1]:
+                seq.append(nums[k])
+            else:
+                i = bisect.bisect_left(seq, nums[k])
+                seq[i] = nums[k]
+        return len(seq)
+
+    def lengthOfLISDP(self, nums: List[int]) -> int:
+        """
+        Given an integer array nums, return the length of the longest strictly
+        increasing subsequence.
+
+        A subsequence is an array that can be derived from another array by deleting
+        some or no elements without changing the order of the remaining elements.
+        """
+        # DP (LIS) Solution: O(n²)
         dp = [1] * (len(nums))
         for i in range(len(nums) - 2, -1, -1):
             for j in range(i + 1, len(nums)):
--- tests/test_300_longest_increasing_subsequence.py
+++ tests/test_300_longest_increasing_subsequence.py
@@ -13,7 +13,21 @@ from awesome_python_leetcode._300_longest_increasing_subsequence import Solution
         ([7, 7, 7, 7, 7, 7, 7], 1),
     ],
 )
-def test_func(nums: List[int], expected: int):
+def test_func_bs(nums: List[int], expected: int):
     """Tests the solution of a LeetCode problem."""
-    length = Solution().lengthOfLIS(nums)
+    length = Solution().lengthOfLISBS(nums)
+    assert length == expected
+
+
+@pytest.mark.parametrize(
+    argnames=["nums", "expected"],
+    argvalues=[
+        ([10, 9, 2, 5, 3, 7, 101, 18], 4),
+        ([0, 1, 0, 3, 2, 3], 4),
+        ([7, 7, 7, 7, 7, 7, 7], 1),
+    ],
+)
+def test_func_dp(nums: List[int], expected: int):
+    """Tests the solution of a LeetCode problem."""
+    length = Solution().lengthOfLISDP(nums)
     assert length == expected
